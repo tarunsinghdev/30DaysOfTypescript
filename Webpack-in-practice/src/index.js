35940c648f027a0066c43ebc453cfa4e83786a43
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-06-08 21:44:25 +0530

    Webpack-in-practice

--- Webpack-in-practice/src/index.js
+++ Webpack-in-practice/src/index.js
@@ -3,6 +3,3 @@ import addImage from './add-image.js';
 
 helloWorld();
 addImage();
-// https://dev.to/smelukov/webpack-5-asset-modules-2o3h
-// https://medium.com/@raviroshan.talk/webpack-understanding-the-publicpath-mystery-aeb96d9effb1
-// https://webpack.js.org/guides/getting-started/
